6e28e35cb5453b7b7a3e22e20f4dd391d505bb8a
jeff <jeff@granularit.com>
2023-11-17 10:35:13 +0300

    Rerender response data on helper functions

--- actions/get-billboards.tsx
+++ actions/get-billboards.tsx
@@ -3,9 +3,12 @@ import { Billboard } from "@/types";
 const url = `${process.env.NEXT_PUBLIC_API_URL}/billboards`;
 
 const getBillboard = async (id: string): Promise<Billboard> => {
-    const res = await fetch(`${url}/${id}`);
+    const res = await fetch(`${url}/${id}`)
+                            .then(response => {
+                                return response.json()
+                            })
 
-    return res.json() || null;
+    return res || null;
 };
 
 export default getBillboard;
--- actions/get-categories.tsx
+++ actions/get-categories.tsx
@@ -3,9 +3,11 @@ import { Category } from "@/types";
 const url = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
 const getCategories = async (): Promise<Category[]> => {
-    const res = await fetch(url);
-
-    return res.json() || null;
+    const res = await fetch(url)
+                            .then((response) => {
+                                return response.json()
+                            });
+    return res || null;
 }
 
 export default getCategories;
