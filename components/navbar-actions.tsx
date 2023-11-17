e4b3df090c9f962883085186211f1502a60bdef5
jeff <jeff@granularit.com>
2023-11-17 10:06:17 +0300

    Invoke a rerender on navbar actions

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -4,9 +4,20 @@ import { ShoppingBag } from "lucide-react";
 
 import Button from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const NavbarActions = () => {
+    const [isMounted, setIsMounted] = useState(false);
     const router = useRouter();
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
+
 
     return (
         <div className="ml-auto flex-items-center gap-x-4">
