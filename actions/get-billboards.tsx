dea1fb2cc498f0e1cf92c9bf17d513271c605a71
jeff <jeff@granularit.com>
2023-11-17 16:44:23 +0300

    Memoize and revalidate data on request

--- actions/get-billboards.tsx
+++ actions/get-billboards.tsx
@@ -1,14 +1,17 @@
+import { cache } from "react";
+
 import { Billboard } from "@/types";
+
+export const revalidate = 3600;
 
 const url = `${process.env.NEXT_PUBLIC_API_URL}/billboards`;
 
-const getBillboard = async (id: string): Promise<Billboard> => {
+const getBillboard = cache(async (id: string): Promise<Billboard> => {
     const res = await fetch(`${url}/${id}`)
                             .then(response => {
                                 return response.json()
                             })
-    console.log(res)
     return res || null;
-};
+});
 
 export default getBillboard;
--- actions/get-categories.tsx
+++ actions/get-categories.tsx
@@ -1,14 +1,17 @@
+import { cache } from "react";
+
 import { Category } from "@/types";
+
+export const revalidate = 3600;
 
 const url = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
-const getCategories = async (): Promise<Category[]> => {
+const getCategories = cache(async (): Promise<Category[]> => {
     const res = await fetch(url, { next: { revalidate: 3600 } } )
                             .then((response) => {
                                 return response.json()
                             });
-    console.log(url)
     return res || null;
-}
+})
 
 export default getCategories;
