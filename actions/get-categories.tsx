18b579c3853b39bc88196e92837d499b0eec6d78
jeff <jeff@granularit.com>
2023-11-17 11:51:00 +0300

    Reavalidate data correclty and omit force-cache as it renders default

--- actions/get-categories.tsx
+++ actions/get-categories.tsx
@@ -5,7 +5,7 @@ import { Category } from "@/types";
 const url = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
 const getCategories = async (): Promise<Category[]> => {
-    const res = await fetch(url, { cache: 'force-cache'} )
+    const res = await fetch(url, { next: { revalidate: 3600 } } )
                             .then((response) => {
                                 return response.json()
                             });
