393233ecb7a1e752e00ff1a77afa500c3a680e20
jeff <jeff@granularit.com>
2023-11-17 18:19:54 +0300

    Request data using axios

--- actions/get-categories.tsx
+++ actions/get-categories.tsx
@@ -1,4 +1,4 @@
-import { cache } from "react";
+import axios, { AxiosResponse } from "axios";
 
 import { Category } from "@/types";
 
@@ -6,12 +6,20 @@ export const revalidate = 3600;
 
 const url = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
-const getCategories = cache(async (): Promise<Category[]> => {
-    const res = await fetch(url, { next: { revalidate: 3600 } } )
-                            .then((response) => {
-                                return response.json()
-                            });
-    return res || null;
-})
+const getCategories = async (): Promise<Category[]> => {
+    try {
+        const res: AxiosResponse<Category[]> = await axios.get<Category[]>(url);
+
+        if (res.status !== 200) {
+            throw new Error(`API request failed with status code ${res.status}`);
+        }
+
+        const data = res.data
+        return data;
+    } catch (error) {
+        console.error("Error fetching categoory data:", error);
+        throw error;
+    }
+};
 
 export default getCategories;
