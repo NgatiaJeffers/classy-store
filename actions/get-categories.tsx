db567a60733068ff82912ef9605b0be9dfeefd0d
jeff <jeff@granularit.com>
2023-11-17 16:39:09 +0300

    Rebuild to produce error on prod

--- actions/get-categories.tsx
+++ actions/get-categories.tsx
@@ -2,17 +2,13 @@ import { Category } from "@/types";
 
 const url = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
-// const getCategories = async (): Promise<Category[]> => {
-//     const res = await fetch(url, { next: { revalidate: 3600 } } )
-//                             .then((response) => {
-//                                 return response.json()
-//                             });
-//     console.log(url)
-//     return res || null;
-// }
-
-const getCategories = async () => {
-    console.log("I a called")
+const getCategories = async (): Promise<Category[]> => {
+    const res = await fetch(url, { next: { revalidate: 3600 } } )
+                            .then((response) => {
+                                return response.json()
+                            });
+    console.log(url)
+    return res || null;
 }
 
 export default getCategories;
--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ import Container from "@/components/ui/container";
 
 
 const HomePage = async () => {
-  // const billboard = await getBillboard("6554cde8dab85d0b80592b2d");
-  // console.log(billboard)
+  const billboard = await getBillboard("6554cde8dab85d0b80592b2d");
+  console.log(billboard)
   return (
     <div>
       <Container>
         <div className="space-y-10 pb-10">
-          {/* <Billboard data={billboard} /> */}
+          <Billboard data={billboard} />
         </div>
       </Container>
     </div>
