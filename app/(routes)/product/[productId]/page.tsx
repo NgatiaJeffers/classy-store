9565c1a81df3f49e6e19a62cc882c09ec5c2bd0f
jeff <jeff@granularit.com>
2023-11-27 14:07:06 +0300

    [feat] Create info page to populate data for product

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -4,6 +4,7 @@ import getProducts from "@/actions/get-products";
 import Container from "@/components/ui/container";
 import ProductList from "@/components/products-list";
 import Gallery from "@/components/ui/gallery";
+import Info from '@/components/info';
 
 interface ProductPageProps {
     params: {
@@ -29,6 +30,9 @@ const ProductPage: React.FC<ProductPageProps> = async ({
                 <div className="px-4 py-10 sm:px-6 lg:px-8">
                     <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
                         <Gallery images={product?.images} />
+                        <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+                            <Info data={product} />
+                        </div>
                     </div>
                     <hr className="my-10" />
                     <ProductList title="Related Items" items={suggestedProducts} />
